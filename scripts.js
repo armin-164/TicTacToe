dd2fe8cdf3934d7b45d40d90ef975ddab7796343
Armin Sadr <arminsadr164@hotmail.com>
2023-03-05 02:02:16 +0100

    Add a factory function
    Add a factory function that fetches the container,
    creates an array for playermoves.
    Add a function inside the factory which creates
    the squares. Append the squares to the gameboard
    container.
    Return the createGameBoard function so it can be
    used outside the factory.

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const gameBoard = () => {
+  const gameBoardContainer = document.querySelector(".game-board-container");
+  const gameBoardArray = [];
+
+  const createGameBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      let boardSquare = document.createElement("div");
+
+      boardSquare.classList.add("game-board-square");
+      boardSquare.setAttribute("data-index", i);
+    }
+    gameBoardContainer.appendChild(boardSquare);
+  };
+  return { createGameBoard };
+};
