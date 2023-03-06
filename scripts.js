f87f1b11af9426fc75fcea8080922f5d8383df25
Armin Sadr <arminsadr164@hotmail.com>
2023-03-06 13:38:53 +0100

    Update the JS and add a checkWinner function
    Move the winnerLogic inside the gameBoard module.
    Create a checkWinner function that declares a winner by looking
    at every combo in the winnerLogic and sees if it matches the index
    of the gameBoardArray. Also add an else if statement when none
    of the combos match (declare a tie).
    
    Create two random players with different names and moves in the
    playGame module.
    
    In the addeventlistener, add an if else statement that checks
    who has the currentmove and then run checkWinner to see if they have
    won.

--- scripts.js
+++ scripts.js
@@ -2,10 +2,43 @@ const gameBoard = (() => {
   const gameBoardContainer = document.querySelector(".game-board-container");
   const gameBoardArray = ["", "", "", "", "", "", "", "", ""];
 
+  let winnerLogic = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // This function takes marker and player as parameter
+  // It goes through winnerLogic and "finds" the combo
+  // by checking the indexes from the combo ON the gameBoardArray
+  // and sees if it equals to the marker
+  // If winner is true, alert that the player has won
+  // Else if gameboardarray isnt empty, alert its a tie since
+  // the previous if statement checks for a winner everytime
+  const checkWinner = (marker, player) => {
+    const winner = winnerLogic.find((combo) =>
+      combo.every((i) => gameBoardArray[i] === marker)
+    );
+    if (winner) {
+      alert(`${player} has won`);
+    } else if (!gameBoardArray.includes("")) {
+      alert("its a tie");
+    }
+  };
+
+  // This function will save the move at a specific index in gameBoardArray
   const saveMove = (index, move) => {
     gameBoardArray[index] = move;
   };
 
+  // This function creates the DOM gameboard
   const createGameBoard = () => {
     for (let i = 0; i < 9; i++) {
       let boardSquare = document.createElement("div");
@@ -16,17 +49,18 @@ const gameBoard = (() => {
       gameBoardContainer.appendChild(boardSquare);
     }
   };
-  return { createGameBoard, saveMove};
+  return { createGameBoard, saveMove, checkWinner };
 })();
 
 const createPlayer = (name, move) => {
   return { name, move };
 };
 
-
-
 const playGame = (() => {
   gameBoard.createGameBoard();
+
+  const jeff = createPlayer("jeff", "X");
+  const joe = createPlayer("joe", "O");
 
   let allBoardSquares = document.querySelectorAll(".game-board-square");
   let currentMove = "X";
@@ -39,21 +73,14 @@ const playGame = (() => {
         currentMove = currentMove === "X" ? "O" : "X";
         square.innerText = currentMove;
         gameBoard.saveMove(square.dataset.index, currentMove);
+
+        if (currentMove === jeff.move) {
+          gameBoard.checkWinner(currentMove, jeff.name);
+        } else if (currentMove === joe.move) {
+          gameBoard.checkWinner(currentMove, joe.name);
+        }
       }
     });
   });
 })();
 
-let winnerLogic = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-
-  [0, 4, 8],
-  [2, 4, 6],
-];
-console.log(winnerLogic);
